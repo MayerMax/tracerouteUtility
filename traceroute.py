3d78ec53e29a64a6f4d1b7d5ae0ab20063fbc7ba
Maxim <endurancemayer@yandex.ru>
2017-03-26 20:37:57 +0500

    troubles with tracerouting

--- traceroute.py
+++ traceroute.py
@@ -10,13 +10,14 @@ from utils import icmp_requests as requests
 import struct
 
 protocol_name = "icmp"
-
+socket.getprotobyname(protocol_name)
 
 class Packet:
-    def __init__(self, version="bbHHh", m_type=requests['echo request'],
+    def __init__(self, version="bbHHh", m_type=requests['echo request'], code=0,
                  checksum=0, id_f=0, sequence=1):
         self.version = version
         self.type = m_type
+        self.code = code
         self.checksum = checksum
         self.id = id_f
         self.sequence = sequence
@@ -24,7 +25,8 @@ class Packet:
     def _bin_packet(self):
         """forming a raw packet with 0 checksum,
         just for further computation of checksum based on prepared binary data"""
-        return struct.pack(self.version, self.type, self.checksum, self.id, self.sequence)
+        return struct.pack(self.version, self.type, self.code,
+                           self.checksum, self.id, self.sequence)
 
     @classmethod
     def check_sum_forming(cls, word):
@@ -51,9 +53,10 @@ class Packet:
     def bin_packet(self, time_id):
         raw_header = self._bin_packet()
         word = struct.pack('d', time.time())  # 8 byte
+        
         pack_checksum = self.check_sum_forming(raw_header + word)  # 8 byte
 
-        packet = Packet(checksum=pack_checksum, id_f=time_id)
+        packet = Packet(checksum=socket.htons(pack_checksum), id_f=time_id)
         return packet._bin_packet()
 
 
@@ -63,28 +66,31 @@ def get_arg_parser():
     parser.add_argument("-t", "--ttl", help="custom ttl bound", default=25, type=int)
 
     return parser
+
+
 def prepare_socket(ttl):
-    query_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,
-                                 socket.getprotobyname(protocol_name))
+    query_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW )
     query_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
-    #query_socket.settimeout() for futher progress
+    # query_socket.settimeout() for futher progress
     return query_socket
 
 
 def traceroute(dest, max_hops):
     try:
         end_point = socket.gethostbyname(dest)
-
+        print(end_point)
         for step in range(1, max_hops):
             sending_socket = prepare_socket(step)
-            packet = Packet().bin_packet(int(id(step)*random.random()%65535))
+            packet = Packet().bin_packet(int(id(step) * random.random() % 65535))
             # sending our packet
-            sending_socket.sendto(packet, address=(end_point, 1))
-
+            sending_socket.sendto(packet, (end_point, 33434))
+            info = receive_packet_timeout(sending_socket, timeout=2)
+            print(info)
 
     except socket.gaierror as s_error:
         print(s_error.args)
+
 
 def receive_packet_timeout(sock, timeout=1):
     start_time = time.time()
@@ -96,16 +102,17 @@ def receive_packet_timeout(sock, timeout=1):
         icmp_header = icmp_message[20:28]
 
         icmpType, code, checksum, packetID, sequence = struct.unpack('bbHHh', icmp_header)
-
+        if icmpType == requests['ttl_expired'] and code == 0:
+            return addr, None
+        if icmpType == requests['echo reply'] and code == 0:
+            return addr, 1
 
 
 if __name__ == "__main__":
-    #arg_parser = get_arg_parser().parse_args()
-    traceroute("yandex.ru")
+    # arg_parser = get_arg_parser().parse_args()
+    traceroute("google.com", 30)
     # traceroute(arg_parser.parse_args())
     # print(socket.gethostbyname("dubrovin"))
     # print(socket.gethostbyname_ex("yandex.ru"))
     # print(socket.gethostbyaddr('yandex.ru'))
     # print(socket.getprotobyname(protocol_name))
-
-
